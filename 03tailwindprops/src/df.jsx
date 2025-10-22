976323ef7100569aa7365ace40121e155fd0847f
Elakshi-01 <elakshiojha7@gmail.com>
2025-10-23 00:06:19 +0530

    add

---
+++ 03tailwindprops/src/df.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+
+
+
+
+
+// function Card({a,b}) {
+// // now f and m is passed in card in main.jsx now they are object states we can acces like
+// console.log(props.f);
+// console.log(a);
+
+//   return (
+//     <div>
+//         <p>lorem100</p>
+//         <h1>ffff</h1>
+//     </div>
+  
+//   )
+// }
+
+
+
+
+function Card(props) {
+// now f and m is passed in card in main.jsx now they are object states we can acces like
+console.log(props.f);
+
+  return (
+    <div>
+        <p>lorem100</p>
+        <h1>ffff</h1>
+    </div>
+  
+  )
+}
+
+
+
+
+
+
+
+export default Card
